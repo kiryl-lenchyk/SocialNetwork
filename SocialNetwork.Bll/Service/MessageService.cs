51abd38c27fffd46a46dde7a19f46e8ae4ca864e
kiryl-lenchyk <kiryl.lenchyk@gmail.com>
2015-06-16 13:38:39 +0300

    Logger added to MessageService. Logger added to Bll

--- SocialNetwork.Bll/Service/MessageService.cs
+++ SocialNetwork.Bll/Service/MessageService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using NLog;
 using SocialNetwork.Bll.Interface.Entity;
 using SocialNetwork.Bll.Interface.Services;
 using SocialNetwork.Bll.Mappers;
@@ -17,6 +18,8 @@ namespace SocialNetwork.Bll.Service
         private readonly IRepository<DalMessage> messageRepository;
         private readonly IUserRepository userRepository;
 
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
         public MessageService(IUnitOfWork uow, IRepository<DalMessage> messageRepository,
             IUserRepository userRepository)
         {
@@ -27,12 +30,16 @@ namespace SocialNetwork.Bll.Service
 
         public BllMessage GetById(int id)
         {
+            Logger.Trace("MessageService.GetById invoked id = {0}", id);
+
             DalMessage dalMessage = messageRepository.GetById(id);
             return dalMessage == null ? null : dalMessage.ToBllMessage();
         }
 
         public IEnumerable<BllDialog> GetUserDialogs(int userId)
         {
+            Logger.Trace("MessageService.GetUserDialogs invoked userId = {0}", userId);
+
             DalUser currentDalUser = userRepository.GetById(userId);
             if (currentDalUser == null)
                 throw new ArgumentException(string.Format("User with id = {0} not found", userId));
@@ -51,6 +58,7 @@ namespace SocialNetwork.Bll.Service
         {
             if(firstUser == null) throw new ArgumentNullException("firstUser");
             if (secondUser == null) throw new ArgumentNullException("secondUser");
+            Logger.Trace("MessageService.GetUsersDialog invoked firstUser = {0}, secondUser = {1} ", firstUser, secondUser);
 
             return new BllDialog
             {
@@ -67,6 +75,7 @@ namespace SocialNetwork.Bll.Service
         public void CreateMessage(BllMessage message)
         {
             if (message == null) throw new ArgumentNullException("message");
+            Logger.Trace("MessageService.CreateMessage invoked sender = {0}, target = {1}", message.SenderId, message.TargetId);
 
             messageRepository.Create(message.ToDalMessage());
             uow.Commit();
@@ -76,6 +85,7 @@ namespace SocialNetwork.Bll.Service
         {
             if (message == null) throw new ArgumentNullException("message");
             if (editorName == null) throw new ArgumentNullException("editorName");
+            Logger.Trace("MessageService.EditMessage invoked id = {0}", message.Id);
 
             message.Text += String.Format("\r\n[Text edited by {0} at {1}]", editorName,
                 DateTime.Now.ToString("g"));
@@ -87,6 +97,7 @@ namespace SocialNetwork.Bll.Service
         {
             if (message == null) throw new ArgumentNullException("message");
             if (editorName == null) throw new ArgumentNullException("editorName");
+            Logger.Trace("MessageService.DeleteMessage invoked id = {0}", message.Id);
 
             message.Text = String.Format("[Message deleted by {0} at {1}]", editorName,
                 DateTime.Now.ToString("g"));
@@ -96,6 +107,8 @@ namespace SocialNetwork.Bll.Service
 
         public int GetUserNotReadedMessagesCount(int userId)
         {
+            Logger.Trace("MessageService.GetUserNotReadedMessagesCount invoked userId = {0}", userId);
+
             return
                 messageRepository.GetAllByPredicate(x => x.TargetId == userId && !x.IsReaded)
                     .Count();
@@ -104,6 +117,7 @@ namespace SocialNetwork.Bll.Service
         public void MarkAsReaded(BllMessage message)
         {
             if (message == null) throw new ArgumentNullException("message");
+            Logger.Trace("MessageService.MarkAsReaded invoked id = {0}", message.Id);
 
             message.IsReaded = true;
             messageRepository.Update(message.ToDalMessage());
@@ -112,6 +126,8 @@ namespace SocialNetwork.Bll.Service
 
         public IEnumerable<BllMessage> GetAllMessages()
         {
+            Logger.Trace("MessageService.GetAllMessages invoked");
+
             return
                 messageRepository.GetAll()
                     .OrderByDescending(x => x.CreatingTime).ToList()
