85709e0feb229b75b211aa874bbf0f1ce50a341b
kiryl-lenchyk <kiryl.lenchyk@gmail.com>
2015-07-13 14:13:39 +0300

    Merged with avatars_in_db

---
+++ SocialNetwork.Dal/ExpressionMappers/AvatarExpressionMapper.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Reflection;
+using SocialNetwork.Dal.Interface.DTO;
+using SocialNetwork.Orm;
+
+namespace SocialNetwork.Dal.ExpressionMappers
+{
+
+    /// <summary>
+    /// ExpressionMapper to change Expression with DalAvatar arguments  Expression with User arguments
+    /// </summary>
+    class AvatarExpressionMapper : GenericExpressionMapper<DalAvatar, User>
+    {
+        private static readonly Dictionary<MemberInfo, LambdaExpression> Mappings;
+
+        static AvatarExpressionMapper()
+        {
+            Mappings = new Dictionary<MemberInfo, LambdaExpression>();
+
+            KeyValuePair<MemberInfo, LambdaExpression> mapping =
+                GetMappingFor(dalAvatar => dalAvatar.UserId,
+                    user => user.Id);
+            Mappings.Add(mapping.Key, mapping.Value);
+
+            mapping = GetMappingFor(dalAvatar => dalAvatar.ImageBytes,
+                user => user.Avatar);
+            Mappings.Add(mapping.Key, mapping.Value);
+
+        }
+
+        /// <summary>
+        /// Create new instanse of AvatarExpressionMapper.
+        /// </summary>
+        internal AvatarExpressionMapper()
+            : base(Mappings)
+        {
+        }
+    }
+}
